4e210411dffc4ead4f880f7813b12ecaef05b0f9
SafdarAliShah <programmersafdar@live.com>
2024-10-04 23:02:51 -0700

    ExpressJS::Server Running

--- src/index.mjs
+++ src/index.mjs
@@ -1,2 +1,9 @@
 import express from "express"; // import express
+
 const app = express(); // initialize express object
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Running on Port ${PORT}`);
+});
