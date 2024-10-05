fbfb51e40a86e75367bc2a4f05150bc9e0300fa4
SafdarAliShah <programmersafdar@live.com>
2024-10-05 00:03:12 -0700

    Single User route added

--- src/index.mjs
+++ src/index.mjs
@@ -9,5 +9,47 @@ app.listen(PORT, () => {
 });
 // Resquest Handler
 app.get("/", (request, response) => {
-  response.send();
+  response.status(201).send({
+    message:
+      "Iran Seeks Pakistan’s Help to Train Fighter Pilots, Strengthening Military Ties",
+  });
+});
+//Show Users Route all users record
+app.get("/users", (request, response) => {
+  response.status(200).send([
+    {
+      id: 1,
+      username: "Safdar Ali Shah",
+      email: "programmersafdar@live.com",
+      password: "@tiger::007@",
+      age: 30,
+    },
+    {
+      id: 2,
+      username: "Azhar Ali Shah",
+      email: "programmerazhar@live.com",
+      password: "@tiger::007@",
+      age: 30,
+    },
+
+    {
+      id: 3,
+      username: "Ather Ali Shah",
+      email: "programmerAther@live.com",
+      password: "@tiger::007@",
+      age: 30,
+    },
+    {
+      id: 4,
+      username: "Mazhar Ali Shah",
+      email: "programmerMazhar@live.com",
+      password: "@tiger::007@",
+      age: 30,
+    },
+  ]);
+});
+//single user route parametter record
+app.get("/users/:id", (request, response) => {
+  console.log(request.params);
+  response.status(200).send();
 });
