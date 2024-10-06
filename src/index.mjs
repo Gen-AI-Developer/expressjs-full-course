9f739e37a527dbc4cdf1dcc2118c6f7286c4f89e
SafdarAliShah <programmersafdar@live.com>
2024-10-05 23:04:27 -0700

    adding Data Validation

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,38 @@
 import express from "express"; // import express
-
 const app = express(); // initialize express object
 
 const PORT = process.env.PORT || 3000;
+const mockUsersData = [
+  {
+    id: 1,
+    username: "Safdar Ali Shah",
+    email: "programmersafdar@live.com",
+    password: "@tiger::007@",
+    age: 30,
+  },
+  {
+    id: 2,
+    username: "Azhar Ali Shah",
+    email: "programmerazhar@live.com",
+    password: "@tiger::007@",
+    age: 30,
+  },
+
+  {
+    id: 3,
+    username: "Ather Ali Shah",
+    email: "programmerAther@live.com",
+    password: "@tiger::007@",
+    age: 30,
+  },
+  {
+    id: 4,
+    username: "Mazhar Ali Shah",
+    email: "programmerMazhar@live.com",
+    password: "@tiger::007@",
+    age: 30,
+  },
+];
 
 app.listen(PORT, () => {
   console.log(`Running on Port ${PORT}`);
@@ -16,7 +46,7 @@ app.get("/", (request, response) => {
 });
 //Show Users Route all users record
 app.get("/users", (request, response) => {
-  response.status(200).send();
+  response.status(200).send(mockUsersData);
 });
 //single user route parametter record
 app.get("/users/:id", (request, response) => {
