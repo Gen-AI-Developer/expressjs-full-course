6da8ffe3a7305e5499d5c035a4380e208c621bb8
SafdarAliShah <programmersafdar@live.com>
2024-10-05 22:42:05 -0700

    Adding Users Data in File

---
+++ src/Data.js
@@ -0,0 +1,31 @@
+const mockUsersData = [
+  {
+    id: 1,
+    username: "Safdar Ali Shah",
+    email: "programmersafdar@live.com",
+    password: "@tiger::007@",
+    age: 30,
+  },
+  {
+    id: 2,
+    username: "Azhar Ali Shah",
+    email: "programmerazhar@live.com",
+    password: "@tiger::007@",
+    age: 30,
+  },
+
+  {
+    id: 3,
+    username: "Ather Ali Shah",
+    email: "programmerAther@live.com",
+    password: "@tiger::007@",
+    age: 30,
+  },
+  {
+    id: 4,
+    username: "Mazhar Ali Shah",
+    email: "programmerMazhar@live.com",
+    password: "@tiger::007@",
+    age: 30,
+  },
+];
--- src/index.mjs
+++ src/index.mjs
@@ -16,37 +16,7 @@ app.get("/", (request, response) => {
 });
 //Show Users Route all users record
 app.get("/users", (request, response) => {
-  response.status(200).send([
-    {
-      id: 1,
-      username: "Safdar Ali Shah",
-      email: "programmersafdar@live.com",
-      password: "@tiger::007@",
-      age: 30,
-    },
-    {
-      id: 2,
-      username: "Azhar Ali Shah",
-      email: "programmerazhar@live.com",
-      password: "@tiger::007@",
-      age: 30,
-    },
-
-    {
-      id: 3,
-      username: "Ather Ali Shah",
-      email: "programmerAther@live.com",
-      password: "@tiger::007@",
-      age: 30,
-    },
-    {
-      id: 4,
-      username: "Mazhar Ali Shah",
-      email: "programmerMazhar@live.com",
-      password: "@tiger::007@",
-      age: 30,
-    },
-  ]);
+  response.status(200).send();
 });
 //single user route parametter record
 app.get("/users/:id", (request, response) => {
